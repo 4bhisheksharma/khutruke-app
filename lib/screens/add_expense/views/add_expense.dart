86f93360fb9416399b37e8793d67206208b9726f
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-12 20:20:24 +0545

    feat: refactor AddExpense screen to use StatefulWidget and implement date selection functionality

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,9 +1,26 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
+
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  DateTime selectDate = DateTime.now();
+
+  @override
+  void initState() {
+    dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +41,9 @@ class AddExpense extends StatelessWidget {
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
                 child: TextFormField(
+                  controller: expenseController,
                   decoration: InputDecoration(
+                    label: Text("Expense Amount"),
                     prefixIcon: Icon(
                       Icons.monetization_on_rounded,
                       color: Colors.grey,
@@ -38,6 +57,7 @@ class AddExpense extends StatelessWidget {
 
               SizedBox(height: 20),
               TextFormField(
+                controller: categoryController,
                 decoration: InputDecoration(
                   label: Text("Category"),
                   prefixIcon: Icon(Icons.list_rounded, color: Colors.grey),
@@ -49,14 +69,23 @@ class AddExpense extends StatelessWidget {
 
               SizedBox(height: 20),
               TextFormField(
+                controller: dateController,
                 readOnly: true,
-                onTap: () {
-                  showDatePicker(
+                onTap: () async {
+                  DateTime? newDate = await showDatePicker(
                     context: context,
-                    initialDate: DateTime.now(),
+                    initialDate: selectDate,
                     firstDate: DateTime.now(),
                     lastDate: DateTime.now().add(Duration(days: 365)),
                   );
+                  if (newDate != null) {
+                    setState(() {
+                      dateController.text = DateFormat(
+                        'dd/MM/yyyy',
+                      ).format(newDate);
+                      selectDate = newDate;
+                    });
+                  }
                 },
                 decoration: InputDecoration(
                   label: Text("Date"),
